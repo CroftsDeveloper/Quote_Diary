69afca0b239fbaf12fcd9ddb3a2d85511efb8800
CroftsDeveloper <crofts.webdeveloper@gmail.com>
2023-11-29 19:26:48 +0000

    Implement User and Quote models setup with Flask-SQLAlchemy
    - Introduced `models.py` to define `User` and `Quote` model classes.
    - Created `extensions.py` to centralise the initialisation of Flask-SQLAlchemy (`db`) and Flask-Migrate (`migrate`).
    - Configured Flask app in `app.py` to use `db` and `migrate` from `extensions.py`.
    - Established a one-to-many relationship between `User` and `Quote` in `models.py`.
    - Adapted `app.py` and `models.py` to use the `db` instance from `extensions.py` to avoid circular imports
    -  initialised and applied database migrations using Flask-Migrate.

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
+# Import the extensions (db and migrate)
+from extensions import db, migrate
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize extensions with the app
+db.init_app(app)
+migrate.init_app(app, db)
+
+# Import models after initializing db and migrate
+from models import User, Quote
 
 @app.route('/')
 def home():
-    return "Welcome to the Quote Diary!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+    return 'Quote Diary'
---
+++ models.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from extensions import db  # Import db from extensions instead of app
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    quotes = db.relationship('Quote', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+class Quote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Quote {self.id} by User {self.author_id}>'
