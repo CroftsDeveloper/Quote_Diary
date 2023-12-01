5681704a7f9fa9913c002f6ca97d6791f6fb25c3
CroftsDeveloper <crofts.webdeveloper@gmail.com>
2023-12-01 22:45:45 +0000

    Enhanced Quote Management with Author Attribution
    - Integrated an author field into the dashboard and edit quote forms.
    
    - Author Tracking:
    Refined the Quote model within models.py, incorporating an 'author_name' field to store author details alongside quotes.
    
    - Modified forms.py to include an author field in QuoteForm, complete with necessary validators
    
    User Interface Enhancements for Author Input and Display:
    
    - Upgraded dashboard.html and edit_quote_form.html with author input fields
    
    Database Migration :
    
    - Executed a migration script to introduce the 'author_name' field in the database

--- models.py
+++ models.py
@@ -14,6 +14,7 @@ class User(db.Model, UserMixin):
 class Quote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
+    author_name = db.Column(db.String(100), nullable=True) 
     author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
